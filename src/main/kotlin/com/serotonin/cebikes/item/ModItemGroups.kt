2fe923218bb4b91f3fabeae90b0bb6be79c96d79
Serotonin/Elliott <elliottbrown6585@gmail.com>
2026-04-04 17:06:53 -0500

    initial commit

---
+++ src/main/kotlin/com/serotonin/cebikes/item/ModItemGroups.kt
@@ -0,0 +1,39 @@
+package com.serotonin.cebikes.item
+
+import com.serotonin.cebikes.registry.CebikesItems
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+
+object ModItemGroups {
+    val CEBIKES_ITEM_GROUP: ItemGroup? = Registry.register(
+        Registries.ITEM_GROUP,
+        Identifier.of("cebikes", "cebikes_items"),
+        FabricItemGroup.builder().icon { ItemStack(CebikesItems.MACH_CORE) }
+            .displayName(Text.translatable("itemgroup.cebikes.cebikes_items"))
+            .entries { _: ItemGroup.DisplayContext?, entries: ItemGroup.Entries? ->
+                entries?.add(CebikesItems.MACH_BIKE)
+                entries?.add(CebikesItems.ACRO_BIKE)
+                entries?.add(CebikesItems.BIKE_RACK)
+                entries?.add(CebikesItems.MACH_CORE)
+                entries?.add(CebikesItems.ACRO_CORE)
+                entries?.add(CebikesItems.MACH_CORE_CENTER)
+                entries?.add(CebikesItems.ACRO_CORE_CENTER)
+                entries?.add(CebikesItems.CORE_TOP_CAP)
+                entries?.add(CebikesItems.CORE_BOTTOM_CAP)
+                entries?.add(CebikesItems.RUBBER_INGOT)
+                entries?.add(CebikesItems.BIKE_FRAME)
+                entries?.add(CebikesItems.BIKE_GEARS)
+                entries?.add(CebikesItems.BIKE_HANDLES)
+                entries?.add(CebikesItems.BIKE_TIRES)
+            }.build()
+    )
+
+    fun registerCebikesItemGroups() {
+        println("registering CebikesItemGroups")
+    }
+}
